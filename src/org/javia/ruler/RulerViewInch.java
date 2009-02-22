5f62f79febe656cfe5ef14d61a5612af02326a03
mpreda <mpreda@d5ec332a-007d-11de-a3ef-1f8437fb274a>
2009-02-22 10:00:31 +0000

    add numbers on rulers

--- src/org/javia/ruler/RulerViewInch.java
+++ src/org/javia/ruler/RulerViewInch.java
@@ -7,17 +7,12 @@ import android.view.View;
 import android.content.Context;
 import android.util.AttributeSet;
 
-public class RulerViewInch extends View {
-    Paint paint = new Paint();
+public class RulerViewInch extends View {    
     static final private float pxinch = 480 / 67.f * 25.4f / 16;
     float width, height;
 
     public RulerViewInch(Context context, AttributeSet foo) {
 	super(context, foo);
-	paint.setStyle(Paint.Style.STROKE);
-	paint.setStrokeWidth(0);
-	paint.setAntiAlias(false);
-	paint.setColor(0xff000000);
     }
 
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
@@ -28,12 +23,15 @@ public class RulerViewInch extends View {
     public void onDraw(Canvas canvas) {
 	canvas.drawColor(0xffffffff);
 	for (int i = 1; ; ++i) {
-	    float y = i * pxinch;
-	    if (y > 480) {
+	    float y = 480 - i * pxinch;
+	    if (y < 0) {
 		break;
 	    }
 	    int size = (i%16==0) ? 45 : (i%8==0) ? 30 : (i%4==0) ? 22 : (i%2==0) ? 15 : 10;
-	    canvas.drawLine(0, y, size, y, paint);
+	    canvas.drawLine(0, y, size, y, Ruler.paint);
 	}
+	canvas.rotate(-90);
+	canvas.drawText("1", -480-10+pxinch*16, 40, Ruler.textPaint);
+	canvas.drawText("2", -480-10+pxinch*32, 40, Ruler.textPaint);
     }
 }
