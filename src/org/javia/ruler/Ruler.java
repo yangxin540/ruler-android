61fd41fa1c869cb227f1cbae128096973cad843b
mpreda <mpreda@d5ec332a-007d-11de-a3ef-1f8437fb274a>
2009-02-22 01:16:33 +0000

    initial

---
+++ src/org/javia/ruler/Ruler.java
@@ -0,0 +1,72 @@
+package org.javia.ruler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.view.MotionEvent;
+import android.util.DisplayMetrics;
+
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+
+public class Ruler extends Activity implements Button.OnClickListener
+{
+    TextView textMM, textInch;
+    Button reset;
+    static private final float mmPerPx = 4.7f;
+    float xdpmm, ydpmm;
+    private double sumX, lastUpdate;
+    ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_RING, 70);
+    int lastmm;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        textMM   = (TextView) findViewById(R.id.mm);
+	textInch = (TextView) findViewById(R.id.inch);
+	reset = (Button) findViewById(R.id.reset);
+	reset.setOnClickListener(this);
+
+	DisplayMetrics dm = new DisplayMetrics();
+	getWindowManager().getDefaultDisplay().getMetrics(dm);
+	// Log.d("Ruler", "" + dm.xdpi + ' ' + dm.ydpi);
+        xdpmm = dm.xdpi / 25.4f;
+	ydpmm = dm.ydpi / 25.4f;
+	reset();
+    }
+
+    private void reset() {
+	sumX = 0;
+	lastUpdate = -1;
+	textMM.setText("0");
+	textInch.setText("0.0");
+	lastmm = 0;
+    }
+
+    public boolean onTrackballEvent(MotionEvent e) {
+	if (e.getAction() == MotionEvent.ACTION_MOVE) {
+	    sumX += e.getX();
+	    if (Math.abs(sumX - lastUpdate) >= 1) {
+		lastUpdate = sumX;
+		toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+	    }
+	}
+	double mm = Math.abs(sumX * mmPerPx);
+	int imm = (int) Math.round(mm);
+	if (lastmm != imm) {
+	    textMM.setText("" + imm);
+	    textInch.setText("" + (Math.round(mm * 10 / 25.4) / 10.));
+	    lastmm = imm;
+	}	
+	return true;
+    }
+
+    public void onClick(View view) {
+	reset();
+    }
+}
